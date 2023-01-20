3e3df5afd5f2265f43db9a362b7b42ed3440db8f
root <root@15-206-145-28.cprapid.com>
2023-01-20 06:52:58 +0000

    update

---
+++ bin/i1.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+
+const greeting = chalk.white.bold("Hello!");
+
+
+
+const boxenOptions = {
+ padding: 1,
+ margin: 1,
+ borderStyle: "round",
+ borderColor: "green",
+ backgroundColor: "#555555"
+};
+
+
+const msgBox = boxen( greeting, boxenOptions );
+
+
+
+const yargs = require("yargs"); 
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: 
+"string", demandOption: true })
+
+ .argv; 
+const greet = `Hello, ${options.name}!`;
+console.log(greet);
+console.log(msgBox);
